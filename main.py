654007a34587affb812897bf93bcef4607c48b13
Hope Brooke <sc21hb@leeds.ac.uk>
2025-04-22 18:25:23 +0100

    added seed to output

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ FOLDER = f'DATA_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}'
 
 # Generate Seed for replicable results
 SEED = npr.randint(0,1000000000)
+SEED = 2246357572
 npr.seed(SEED) # only need to do it here (not for every agent move)
 
 
@@ -52,6 +53,11 @@ def draw_ui(screen, state):
     
     # Draw agents
     for agent in state.petri.agents:
+        if agent.imotile:
+            colour = (0,0,0)
+        else:
+            colour = (229, 89, 52)
+        pygame.draw.circle(screen, colour, (int(agent.x * 2), int(agent.y * 2)), 1)
         pygame.draw.circle(screen, (229, 89, 52), (int(agent.x * 2), int(agent.y * 2)), 1)
     
     # Draw UI buttons
@@ -74,21 +80,20 @@ def draw_ui(screen, state):
 def main():
  
     # Nutrient Grid Parameters:
-    GRID_SIZE = 350  # Square grid dimensions
-    TIME_STEP = 0.01 # Stepwise diffusion rate per loop iteration
-    C_MAX = 1.0      # Maximum nutrient value on a given square
-    D_C = 0.05      # Rate of diffusion
-    
-    # Agent Parameters:
+    GRID_SIZE = 500  # Square grid dimensions
+    TIME_STEP = 1 # Stepwise diffusion rate per loop iteration
+    C_MAX = 1.0
+    D_C = 0.0498
+
     AGENT_PARAMS = {
-        "r_max": 0.1,    # maximum reaction rate
-        "K_m": 0.25,      # michaelis menten constant
-        "m_min": 1,      # minimum mass of agent
-        "delta_H": 12,  # mass to energy rate
-        "F_d": 0.5,    # drag force
-        "mu": 0.8,      # viscosity
-        "p": 0.01,     # nutrient to mass rate
-        "density": 0.08, # density of agent
+        "r_max": 0.0498,
+        "K_m": 0.25,
+        "m_min": 1,
+        "delta_H": 10,
+        "F_d": 0.5,
+        "mu": 0.8,
+        "p": 0.01,
+        "density": 0.08,
     }
     
     # Paper values
@@ -100,12 +105,12 @@ def main():
     print(f'D: {(D_C)/(min_r*v_max)}')
     print(f'E: {(AGENT_PARAMS["F_d"]*min_r)/(AGENT_PARAMS["m_min"]*AGENT_PARAMS["delta_H"])}')
     # Simulation Parameters:
-    max_iters = 50000     # Number of loop iterations for simulation
+    max_iters = 500000     # Number of loop iterations for simulation
     num_agents = 1   # Initial cell count
     mode = 'gif'    # 'vis' for visualisation, 'gif' same but saves images.
 
     # Simulation state (holds all simulation data + petri dish + agents)
-    sim = SimulationState(GRID_SIZE, AGENT_PARAMS, C_MAX, D_C, TIME_STEP, num_agents, max_iters)
+    sim = SimulationState(GRID_SIZE, AGENT_PARAMS, C_MAX, D_C, TIME_STEP, SEED, num_agents, max_iters)
 
     # Create output directory for GIFs
     if mode == 'gif' and not os.path.exists(f"figures/{FOLDER}"):
@@ -113,6 +118,7 @@ def main():
 
     # Start simulation
     pygame.init()
+    
     screen = pygame.display.set_mode((GRID_SIZE*2, GRID_SIZE*2))
     clock = pygame.time.Clock()
     running = True
@@ -122,6 +128,7 @@ def main():
     
     # First screen
     draw_ui(screen, sim)
+    pygame_widgets.update([])
     pygame.display.flip()
 
     # Continue while there are iterations left and the simulation is running
