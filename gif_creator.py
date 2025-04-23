44547e4b55e2a842c8a0a157c2b0518582f61383
Hope Brooke <sc21hb@leeds.ac.uk>
2025-04-23 17:58:36 +0100

    updated gif creator to ignore saved data files

--- gif_creator.py
+++ gif_creator.py
@@ -11,5 +11,9 @@ if not os.path.exists(f"gifs/"):
 
 with imageio.get_writer(f'gifs/{FOLDER}.gif', mode='I') as writer:
     for image in natsorted(os.listdir(images_path)):
+        # make sure file is a png that starts with 'frame'
+        if not image.endswith('.png') and not image.startswith('frame'):
+            continue
+        
         img = imageio.imread(images_path+image)
         writer.append_data(img)
